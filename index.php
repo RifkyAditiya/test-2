65e8a9950a39c19565d2b244badce887f77c8ee5
= <rifky9180@gmail.com>
2023-11-12 21:06:29 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam architecto, delectus cum fugit molestiae labore placeat repudiandae, esse maxime provident similique reprehenderit doloribus sed ipsam illo numquam quod velit ratione.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
